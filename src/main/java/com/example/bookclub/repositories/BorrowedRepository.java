fe07cef4f4fca70d07ea082453faad7ff7f2bae3
Raul Claudiu Guiu <Raul-Claudiu.Guiu@endava.com>
2022-07-11 16:06:37 +0300

    fixed available books

--- src/main/java/com/example/bookclub/repositories/BorrowedRepository.java
+++ src/main/java/com/example/bookclub/repositories/BorrowedRepository.java
@@ -1,6 +1,5 @@
 package com.example.bookclub.repositories;
 
-import com.example.bookclub.models.Book;
 import com.example.bookclub.models.Borrowed;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
--- src/main/java/com/example/bookclub/services/BookService.java
+++ src/main/java/com/example/bookclub/services/BookService.java
@@ -4,6 +4,7 @@ import com.example.bookclub.models.Account;
 import com.example.bookclub.models.Book;
 import com.example.bookclub.models.BookOwner;
 import com.example.bookclub.models.Borrowed;
+import com.example.bookclub.repositories.BookOwnerRepository;
 import com.example.bookclub.repositories.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,7 +17,8 @@ import java.util.stream.Collectors;
 public class BookService {
     @Autowired
     private BookRepository bookRepository;
-
+    @Autowired
+    private BookOwnerRepository bookOwnerRepository;
     public List<Book> getAll() {
         return bookRepository.findAll();
     }
@@ -34,10 +36,11 @@ public class BookService {
     }
 
     public List<Book> getAvailableForRenting() {
-        BookRepository bookRepository1 = bookRepository;
-        List<Book> bookList = new java.util.ArrayList<>(bookRepository1.findAll().stream().filter(b
-                -> b.getBookOwnersList().isEmpty()).toList());
-        bookList.addAll(bookRepository.getAvailableForRenting(LocalDate.now()));
-        return bookList;
+        BookOwnerRepository bookRepository1 = bookOwnerRepository;
+        List<BookOwner> bookList = bookRepository1.findAll().stream().filter(b
+                -> b.getBorrowedList().isEmpty()).toList();
+        List<Book> books = new java.util.ArrayList<>(bookList.stream().map(BookOwner::getBook).toList());
+        books.addAll(bookRepository.getAvailableForRenting(LocalDate.now()));
+        return books;
     }
 }
